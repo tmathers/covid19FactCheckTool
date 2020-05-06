0489af050564a5ae08f0a13b1627faca28b29ad4
Fattema <64709213+Fattema@users.noreply.github.com>
2020-05-06 03:46:38 -0300

    update navigation.php
    removed contact and replaced with test your knowledge and changed the home name to fake-fact check

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,19 +1,16 @@
-<div class="container">
+<div style="margin:0px 100px;" class="container">
 	<ul class="nav nav-pills">
-	  <li class="nav-item">
-	    <a class="nav-link <?php if ($CURRENT_PAGE == "Index") {?>active<?php }?>" href="index.php">Home</a>
+	  <li class="nav-item ">
+	  <a class="nav-link <?php if ($CURRENT_PAGE == "Index") {?>active<?php }?>; btn btn-outline-dark btn-lg"href="index.php" >Fake-Fact Check</a>
 	  </li>
 	  <li class="nav-item">
-	    <a class="nav-link <?php if ($CURRENT_PAGE == "WhatIsIt") {?>active<?php }?>" href="what.php">COVID-19?</a>
+	    <a class="nav-link <?php if ($CURRENT_PAGE == "WhatIsIt") {?>active<?php }?>; btn btn-outline-dark btn-lg" href="what.php">COVID-19</a>
 	  </li>
 	  <li class="nav-item">
-	    <a class="nav-link <?php if ($CURRENT_PAGE == "About") {?>active<?php }?>" href="about.php">About Us</a>
+	    <a class="nav-link <?php if ($CURRENT_PAGE == "About") {?>active<?php }?>; btn btn-outline-dark btn-lg" href="about.php">About Us</a>
 	  </li>
 	  <li class="nav-item">
-	    <a class="nav-link <?php if ($CURRENT_PAGE == "Contact") {?>active<?php }?>" href="contact.php">Contact Us</a>
-	  </li>
-	  <li class="nav-item">
-	    <a class="nav-link <?php if ($CURRENT_PAGE == "Test") {?>active<?php }?>" href="test.php">Test Your Knowledge</a>
+	    <a class="nav-link <?php if ($CURRENT_PAGE == "Test") {?>active<?php }?>; btn btn-outline-dark btn-lg" href="test.php">Test Your Knowledge</a>
 	  </li>
 	</ul>
 </div>
