7f434e60179417d92d6db2ba6a4fc49a0e76814e
Fattema <64709213+Fattema@users.noreply.github.com>
2020-05-05 23:40:14 -0300

    Update to Navigation
    Removed latest news, added test your knowledge

--- includes/navigation.php
+++ includes/navigation.php
@@ -7,13 +7,13 @@
 	    <a class="nav-link <?php if ($CURRENT_PAGE == "WhatIsIt") {?>active<?php }?>" href="what.php">COVID-19?</a>
 	  </li>
 	  <li class="nav-item">
-	    <a class="nav-link <?php if ($CURRENT_PAGE == "News") {?>active<?php }?>" href="news.php">Latest News</a>
-	  </li>
-	  <li class="nav-item">
 	    <a class="nav-link <?php if ($CURRENT_PAGE == "About") {?>active<?php }?>" href="about.php">About Us</a>
 	  </li>
 	  <li class="nav-item">
-	    <a class="nav-link <?php if ($CURRENT_PAGE == "Contact") {?>active<?php }?>" href="contact.php">Contact</a>
+	    <a class="nav-link <?php if ($CURRENT_PAGE == "Contact") {?>active<?php }?>" href="contact.php">Contact Us</a>
+	  </li>
+	  <li class="nav-item">
+	    <a class="nav-link <?php if ($CURRENT_PAGE == "Test") {?>active<?php }?>" href="test.php">Test Your Knowledge</a>
 	  </li>
 	</ul>
 </div>
